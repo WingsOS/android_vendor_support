5197b8b2c80bf7be4816c1bd38615c3c2e6c91e6
someone5678 <someone5678@users.noreply.github.com>
2024-03-11 12:45:44 +0900

    support: Move to SwitchPreferenceCompat
    Change-Id: I1d3b554363bbfc0b4fdec18f48b47690bb751d9a

--- src/com/wings/support/preferences/CustomSwitchPreference.java
+++ src/com/wings/support/preferences/CustomSwitchPreference.java
@@ -19,11 +19,11 @@ package com.wings.support.preferences;
 import android.content.Context;
 import android.util.AttributeSet;
 
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.wings.support.R;
 
-public class CustomSwitchPreference extends SwitchPreference {
+public class CustomSwitchPreference extends SwitchPreferenceCompat {
 
     public CustomSwitchPreference(Context context) {
         super(context);
